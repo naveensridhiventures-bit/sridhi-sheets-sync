6d5c6d487fb2133ebe4bb9af7c6ec3851f0495a5
naveensridhiventures-bit <naveensridhiventures@gmail.com>
2026-07-09 16:40:30 +0530

    fix: new-lead toast wasn't checking on first data load; remove Upgrade to Pro card

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-16ad23c7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.72hpm5tt8po"
+    "revision": "0.2ctfq5f3et"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
